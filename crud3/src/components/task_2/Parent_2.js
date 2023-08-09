e887dc493b789cddd4bbfeeabae2b0d484bfe809
krupaliNimap <krupali@nimapinfotech.com>
2023-08-09 15:55:29 +0530

    task2

--- crud3/src/components/task_2/Parent_2.js
+++ crud3/src/components/task_2/Parent_2.js
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
 
 const Parent2 = () => {
-  const [number, setNumber] = useState();
-  let numDivisible = [];
-  let temp = number;
-  let cubeRoot = 1;
   const [answer, setAnswer] = useState();
-  const isCubeOf = () => {
+  const [number, setNumber] = useState();
+  function isCubeOf() {
+    let list = [];
     for (let i = 2; i <= number; i++) {
-      while (temp % i === 0) {
-        temp = Math.floor(temp / i);
-        numDivisible.push(i);
+      list.push(i);
+      if (i * i * i === number) {
+        break;
+      }
+      if (i * i * i > number) {
+        break;
       }
     }
-    console.log(numDivisible);
-    let list = numDivisible.reduce((acc, curr) => {
-      if (acc[curr]) {
-        acc[curr] = acc[curr] + 1;
-      } else {
-        acc[curr] = 1;
-      }
-      return acc;
-    }, {});
-    for (let i of Object.entries(list)) {
-      let num = Math.floor(i[1] / 3);
-      cubeRoot *= i[0] ** num;
-      setAnswer(cubeRoot);
-    }
-  };
-  console.log("number", number, "cubeRoot", cubeRoot);
-  if (cubeRoot * cubeRoot * cubeRoot === number) {
-    console.log("hello");
+
+    let num1 = list[list.length - 1] ** 3 - number;
+    let num2 = number - list[list.length - 2] ** 3;
+    setAnswer(num1 < num2 ? list[list.length - 1] : list[list.length - 2]);
   }
-  console.log(cubeRoot);
+  // const [number, setNumber] = useState();
+  // const numDivisible = [];
+  // let temp = number;
+  // let cubeRoot = 1;
+  // const [answer, setAnswer] = useState();
+  // // if (number == 1) {
+  // //   setAnswer(1);
+  // // }
+  // const isCubeOf = (number) => {
+  //   if (+number === 1) {
+  //     setAnswer(1);
+  //   }
+  //   for (let i = 2; i <= number; i++) {
+  //     while (temp % i === 0) {
+  //       temp = Math.floor(temp / i);
+  //       numDivisible.push(i);
+  //     }
+  //   }
+  //   let list = numDivisible.reduce((acc, curr) => {
+  //     if (acc[curr]) {
+  //       acc[curr] = acc[curr] + 1;
+  //     } else {
+  //       acc[curr] = 1;
+  //     }
+  //     return acc;
+  //   }, {});
+  //   for (let i of Object.entries(list)) {
+  //     let num = Math.floor(i[1] / 3);
+  //     cubeRoot *= i[0] ** num;
+  //     setAnswer(cubeRoot);
+  //   }
+  // };
   return (
     <div>
       <div>
